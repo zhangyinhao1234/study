9e90b5d3154af0b1457a50207593407dd6a0145f
zhangyinhao <zhangyinhao1234@163.com>
2019-06-02 20:56:59 +0800

    添加客户端发送消息查看返回结果的例子

--- netty/src/main/java/com/github/zhangyinhao1234/study/simple/HelloClientStarup.java
+++ netty/src/main/java/com/github/zhangyinhao1234/study/simple/HelloClientStarup.java
@@ -3,9 +3,13 @@ package com.github.zhangyinhao1234.study.simple;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,6 +20,7 @@ import java.io.InputStreamReader;
  * Date 2019/6/2 16:22
  */
 public class HelloClientStarup {
+    Logger logger = LoggerFactory.getLogger(this.getClass());
 
     String address = "127.0.0.1";
 
@@ -39,7 +44,19 @@ public class HelloClientStarup {
             if (line == null) {
                 continue;
             }
-            ch.writeAndFlush(line);
+            ChannelFuture cf = ch.writeAndFlush(line);
+            //写操作完成后收到的通知
+            cf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    if(future.isSuccess())
+                        logger.debug("想服务器发送消息："+line+"结果：成功");
+                    else
+                        logger.debug("想服务器发送消息："+line+"结果：失败");
+                }
+            });
+
+
         }
     }
 
